e9c86bf62563103b8a173960bfa00fb3eae0db00
Adminn <Adminn@DESKTOP-D12RJV5>
2021-08-12 05:48:49 -0700

    Add project files.

---
+++ Controllers/UserRegisterController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace User_Registration.Controllers
+{
+    public class UserRegisterController : Controller
+    {
+        // GET: UserRegister
+        [HttpGet]
+        public ActionResult RegisterPage()
+        {
+            return View();
+        }
+    }
+}
